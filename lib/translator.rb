f959f7fc1217fb599906abf6d37c709b20337e40
Jared Harbison <jared.harbison@gmail.com>
2019-01-13 20:28:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,14 +24,11 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticon_hash = load_library(path)
-  result = emoticon_hash["get_emoticon"][emoticon]
+  result = emoticon_hash["get_meaning"][emoticon]
   if result == nil 
     result = "Sorry, that emoticon was not found" 
 end
 result
 end
-
-
-
 
 
